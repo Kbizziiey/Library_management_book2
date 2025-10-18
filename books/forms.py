6aaaf9decb0b7897b20c091963d55c41bff73734
Karabo Makganyoga <kamakjhb025@student.wethinkcode.co.za>
2025-10-18 22:25:49 +0200

    added books view

---
+++ books/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'isbn', 'publication_date', 'genre', 'description', 'file']
+        widgets = {
+            'publication_date': forms.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
--- books/views.py
+++ books/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Book
+from .forms import BookForm
+import os
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_detail.html', {'book': book})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/book_form.html', {'form': form})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/book_confirm_delete.html', {'book': book})
+
+def download_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if book.file:
+        response = HttpResponse(book.file, content_type='application/force-download')
+        response['Content-Disposition'] = f'attachment; filename="{os.path.basename(book.file.name)}"'
+        return response
+    return redirect('book_detail', pk=pk)
