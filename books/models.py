c2fd013f0c30a049ab5e68fa0bb05a5727d2148b
Karabo Makganyoga <kamakjhb025@student.wethinkcode.co.za>
2025-10-18 22:23:09 +0200

    added books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+    genre = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    file = models.FileField(upload_to='books/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
